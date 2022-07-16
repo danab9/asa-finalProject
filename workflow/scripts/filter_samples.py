3e007b06672f23b169d560e3f7676b2e42d2ed1d
danab93 <Dnabisgh11>
2022-07-16 12:56:23 +0200

    added black list option

--- workflow/scripts/filter_samples.py
+++ workflow/scripts/filter_samples.py
@@ -10,12 +10,7 @@ black_list=[line.strip() for line in open(snakemake.input["black_list"])] # read
 with open(snakemake.output["filtered"], 'w') as out_file:
     
     for record in SeqIO.parse(snakemake.input["msa"], "fasta"): 
-        id = record.id.strip('_genes') 
-        if id in black_list:
+        if record.id.removesuffix('_genes') in black_list:
             continue # skip if id is on black list 
-        record.id = id
         SeqIO.write(record, out_file, "fasta")  # copy id to new file
-
-
-
 
